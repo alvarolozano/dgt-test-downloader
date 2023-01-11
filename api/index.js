aa531dd8d3ec6ad1719e0374f3985d8d78c20b48
Alvaro <alvarolozano2000@gmail.com>
2023-01-11 23:29:34 +0100

    API security

--- api/index.js
+++ api/index.js
@@ -4,9 +4,13 @@ import getRandomExam from '../download.js';
 
 export default async function handler(request, response) {
         try {
-            response.status(200).json((await getRandomExam()).cuestionario);
+            if(!process.env.RAPIDAPI_KEY || (process.env.RAPIDAPI_KEY && process.env.RAPIDAPI_KEY == request.headers['X-RapidAPI-Proxy-Secret'])) {
+                response.status(200).json((await getRandomExam()).cuestionario);
+            } else {
+                response.status(403).send("No dispones de permiso para ver esta página");
+            }
         } catch {
-            response.status(500);
+            response.status(500).json("Se ha producido un error");
         }
 }
 
