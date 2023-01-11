7366a3ca026511165c98df5cfc75630115318a48
Alvaro <alvarolozano2000@gmail.com>
2023-01-11 23:35:09 +0100

    Proxy header name

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import getRandomExam from '../download.js';
 
 export default async function handler(request, response) {
         try {
-            if(!process.env.RAPIDAPI_KEY || (process.env.RAPIDAPI_KEY && process.env.RAPIDAPI_KEY == request.headers['X-RapidAPI-Proxy-Secret'])) {
+            if(!process.env.RAPIDAPI_KEY || (process.env.RAPIDAPI_KEY && process.env.RAPIDAPI_KEY == request.headers['x-rapidapi-proxy-secret'])) {
                 response.status(200).json((await getRandomExam()).cuestionario);
             } else {
                 response.status(403).send("No dispones de permiso para ver esta página");
